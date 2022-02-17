6208e2eb957cb27c3c7f2e431f98f57ba29efd97
mostafa <mostafa.mm661@gmail.com>
2022-02-17 22:25:32 +0200

    redesign

---
+++ src/main/java/com/drone/dto/ValidDroneDto.java
@@ -0,0 +1,21 @@
+package com.drone.dto;
+
+import com.drone.enums.State;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ValidDroneDto {
+
+    private Integer id;
+    private String serialNumber;
+    private Long weightLimit;
+    private Integer batteryLevel;
+    private State state;
+}
+
